d84da7e0a1a99088939ee74b000623befd81001d
SeoSuHong <tbgkdntm@naver.com>
2022-03-28 00:58:15 +0900

    first commit

---
+++ recursive.py
@@ -0,0 +1,54 @@
+# 재귀 함수 호출
+# for i in range(1, 6):
+#     print(i)
+#     if i == 5:
+#         print("번호 끝")
+
+def count_1(): # 첫 번째 사람
+    print(1)
+    count_2()
+    print("1 끝")
+
+def count_2():
+    print(2)
+    count_3()
+    print("2 끝")
+
+def count_3():
+    print(3)
+    count_4()
+    print("3 끝")
+
+def count_4():
+    print(4)
+    count_5()
+    print("4 끝")
+
+def count_5():
+    print(5)
+    print("번호 끝")
+    print("5 끝")
+
+count_1()
+
+def count(num=1):
+    print(num)
+    if num == 5:
+        print("번호 끝")
+    else:
+        count(num + 1)
+    print(num, "끝")
+
+count()
+
+# factorial 만들기
+def factorial(n):
+    # 4 * 3 * 2 * 1
+    # 4 * 3!
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+result = factorial(4)
+print(result) # 24
